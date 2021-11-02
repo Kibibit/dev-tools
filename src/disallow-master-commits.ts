b9a90419c4e391d2fde8d783133e769868ead1de
thatkookooguy <neil.kalman@growthspace.com>
2021-11-03 00:02:41 +0200

    feat(tests): start covering with tests

--- src/disallow-master-commits.ts
+++ src/disallow-master-commits.ts
@@ -6,10 +6,20 @@ const git = simpleGit();
   try {
     const branchSummaryResult = await git.branch(['-vv']);
     const mainBranch =
-      branchSummaryResult.branches.master || branchSummaryResult.branches.main;
+      branchSummaryResult.branches.master ||
+      branchSummaryResult.branches.main ||
+      branchSummaryResult.branches.develop ||
+      branchSummaryResult.branches.dev ||
+      branchSummaryResult.branches.staging ||
+      branchSummaryResult.branches.next ||
+      branchSummaryResult.branches.beta ||
+      branchSummaryResult.branches.alpha;
 
     if (mainBranch.current === true) {
-      console.info('Should not commit directly to master when working locally');
+      console.info([
+        'Should not commit directly to ',
+        `${ mainBranch.name } when working locally`
+      ].join(''));
       process.exit(1);
     }
     process.exit(0);
